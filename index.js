3a39ce1aec1b3a823ffdab7746275df6206eda81
Cameron Smith <ca.smith657@gmail.com>
2019-06-28 11:41:28 -0600

    added content

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+function getVideoList () {
+	var videos;
+	var sqlString = "SELECT * FROM videoList";
+	pool.query(sqlString, (err, result) => {
+		if (err) {
+			console.log("error in query: ", err);
+		}
+		else {
+			//log to console
+			console.log("Back from db with result: ", result.rows);
+			vidoes = result.rows;
+			return videos;
+		}
+	});
+};
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080;
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: connectionString});
 
-//define functions here
+var videos = getVideoList();
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
   	.set('view engine', 'ejs')
-  	//gets go here
+  	.get('/videos', (req, res) => res.render('landing', {videos}))
+  	.get('*', (req, res) => res.send("video not found", 404))
